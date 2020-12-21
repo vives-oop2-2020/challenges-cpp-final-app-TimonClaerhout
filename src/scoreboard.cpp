59dbb624df59a92badb741d14f0ce87c6cfdd007
TimonClaerhout <timon.claerhout@student.vives.be>
2020-12-21 09:57:10 +0100

    cleaned code from scoreboard to read text file

--- src/scoreboard.cpp
+++ src/scoreboard.cpp
@@ -1,7 +1,5 @@
 #include "scoreboard.h"
 #include <fstream>
-#include <iostream>
-#include <string>
 
 void Scoreboard::save_score(int score){
     // Create and open a text file
@@ -12,13 +10,13 @@ void Scoreboard::save_score(int score){
 }
 
 int Scoreboard::giveHighscore(void){
-    std::string line;
+    int number = 0;
     int highnumber = 0;
     // Read from the text file
     std::ifstream MyReadFile("scoreboard.txt");
-    while (getline (MyReadFile, line)) {
-        if(std::stoi(line) > highnumber){
-            highnumber = std::stoi(line);
+    while (getline (MyReadFile, )) {
+        if(MyReadFile >> number){
+            highnumber = number;
         }
     }
     return highnumber;
